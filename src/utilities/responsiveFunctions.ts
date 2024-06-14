b345319cbdceb6a5e7175aa6a7d454ed41c281ea
Manan <mkumar@innow8apps.com>
2024-06-14 17:20:03 +0530

    "Initial Setup"

---
+++ src/utilities/responsiveFunctions.ts
@@ -0,0 +1,23 @@
+import {Dimensions, PixelRatio} from 'react-native';
+
+const {width, height} = Dimensions.get('screen');
+
+export const screenWidth = width < height ? width : height;
+
+export const screenHeight = width < height ? height : width;
+
+export const responsiveWidth = (size:number) => {
+  const responsiveValue = screenWidth / 375;
+  return responsiveValue * size;
+};
+
+export const responsiveHeight = (size:number) => {
+  const responsiveValue = screenHeight / 812;
+  return responsiveValue * size;
+};
+
+const sizeFactor = 1.25;
+
+export const respFontSize = (size:number) => {
+  return responsiveWidth(size * sizeFactor) / PixelRatio.getFontScale();
+};
